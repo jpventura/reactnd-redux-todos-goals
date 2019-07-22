268450448b22d0dba6f64eb548df490ad4a59210
JP Ventura <contact@jpventura.com>
2019-07-22 16:38:40 -0300

    :sparkles: Listen to store changes

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ function createStore() {
   // 4. Update the state
 
   let state;
+  const listeners = [];
 
   const getState = () => state;
 
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+
   return {
     getState,
+    subscribe,
   };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log('The new state is ', store.getState());
+});
+
+store.subscribe(() => {
+  console.log('The store changed');
+});
 
 export default createStore;
